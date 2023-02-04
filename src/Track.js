ab41b9bdd3ea60605e3b37b2d61d9ddfc737d8ec
Farhan Alam <farhanalam.dev@outlook.com>
2023-02-04 07:49:46 +0000

    Adding interlinking between pages

--- src/Track.js
+++ src/Track.js
@@ -1,11 +1,43 @@
 import React from "react";
+import "./styles.css";
+import logo from "./logo.png";
 
+import { Link } from "react-router-dom";
+import Banner from "./delivery.jpg";
 function Track() {
   return (
     <div>
-      <h1>Track</h1>
+      <div>
+        <nav>
+          <img src={logo} alt="" />
+          <div className="nav-middle">
+            <Link to="/">Home</Link>
+            <Link to="/track">Track</Link>
+            <Link to="/book">Book</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+          <div className="nav-right">
+            <button>Log In</button>
+            <button>Sign Up</button>
+          </div>
+        </nav>
+        <div className="banner">
+          <img src={Banner} alt="" />
+        </div>
+        <div className="overview">
+          <div className="overviewOne">
+            <div className="overviewButton">
+              <button>Track Your Order</button>
+            </div>
+          </div>
+          <div className="overviewOne">
+            <div className="trackSubHead">
+              <label>Track a shipment</label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default Track;
