455d3e025b74914cc934442b92f0e4c428b17a03
Farhan Alam <farhanalam.dev@outlook.com>
2023-02-04 09:11:19 +0000

    adding contact page basic structure

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,45 @@
 import React from "react";
+import "./styles.css";
+import logo from "./logo.png";
 
+import { Link } from "react-router-dom";
+import Banner from "./delivery.jpg";
 function Contact() {
   return (
     <div>
-      <h1>Contact</h1>
+      <div>
+        <nav>
+          <img src={logo} alt="" />
+          <div className="nav-middle">
+            <Link to="/">Home</Link>
+            <Link to="/track">Track</Link>
+            <Link to="/book">Book</Link>
+            <Link to="/contact" className="active">
+              Contact
+            </Link>
+          </div>
+          <div className="nav-right">
+            <button>Log In</button>
+            <button>Sign Up</button>
+          </div>
+        </nav>
+        <div className="banner">
+          <img src={Banner} alt="" />
+        </div>
+        <div className="overview">
+          <div className="overviewOne">
+            <div className="overviewButton">
+              <button>Track Your Order</button>
+            </div>
+          </div>
+          <div className="overviewOne">
+            <div className="trackSubHead">
+              <label>Connect with us</label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default Contact;
